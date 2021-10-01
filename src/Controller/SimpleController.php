dbee0cf4dc84572ae38e7dba12a5d03c03eaf33f
Vlad Stelmakh <vlad1k9369@gmail.com>
2021-10-02 01:11:58 +0300

    some code

--- src/Controller/SimpleController.php
+++ src/Controller/SimpleController.php
@@ -1,18 +1,31 @@
 <?php
+
+declare(strict_types=1);
+
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SimpleController
 {
-    #@Route("/api/number/{max}", name="app_lucky_number")
-    public function number(int $max): Response
+    #[Route('/api/number/{max}')]
+    public function number(int $max): JsonResponse
     {
         $number = random_int(0, $max);
+        return new JsonResponse(['Lucky number' => $number, 'success' => false]);
+    }
 
-        return new Response(
-        '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+    #[Route('/api/double/{num}')]
+    public function double(int $num): JsonResponse
+    {
+        $result =  $num * $num;
+        return new JsonResponse(['result' => $result]);
+    }
+
+    #[Route('/api/sqrt/{num}')]
+    public function sqrt(int $num): JsonResponse
+    {
+        return new JsonResponse(['result' => sqrt($num)]);
     }
 }
